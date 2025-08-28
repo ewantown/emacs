fa6de0c6b66aed020c015de2d7c56d4352e89e74
ewantown <you@example.com>
2025-08-28 00:21:51 -0700

    restored NT variant of term color setup

--- src/term.c
+++ src/term.c
@@ -2206,7 +2206,7 @@ TERMINAL does not refer to a text terminal.  */)
   return make_fixnum (t ? t->display_info.tty->TN_max_colors : 0);
 }
 
-#if !defined DOS_NT && !defined HAVE_ANDROID
+#if !defined MSDOS && !defined HAVE_ANDROID
 
 /* Declare here rather than in the function, as in the rest of Emacs,
    to work around an HPUX compiler bug (?). See
@@ -2264,41 +2264,41 @@ tty_setup_colors (struct tty_display_info *tty, int mode)
 	tty_default_color_capabilities (tty, 0);
 	break;
       case 8: /* 8 standard ANSI colors */
-	tty->TS_orig_pair = "\033[0m";
-	tty->TS_set_foreground = "\033[3%dm";
-	tty->TS_set_background = "\033[4%dm";
+	tty->TS_orig_pair = "\x1b[0m";
+	tty->TS_set_foreground = "\x1b[3%dm";
+	tty->TS_set_background = "\x1b[4%dm";
 #ifdef TERMINFO
-	tty->TS_set_foreground = "\033[3%p1%dm";
-	tty->TS_set_background = "\033[4%p1%dm";
+	tty->TS_set_foreground = "\x1b[3%p1%dm";
+	tty->TS_set_background = "\x1b[4%p1%dm";
 #endif
-/* #ifdef WINDOWSNT */
-/* 	tty->TS_orig_pair = "[39m[49m"; */
-/* 	tty->TS_set_foreground = "[%dm"; */
-/* 	tty->TS_set_background = "[%dm"; */
-/* #endif */
+#ifdef WINDOWSNT
+	tty->TS_orig_pair = "\x1b[39m\x1b[49m";
+	tty->TS_set_foreground = "\x1b[%dm";
+	tty->TS_set_background = "\x1b[%dm";
+#endif
 	tty->TN_max_colors = 8;
 	tty->TN_no_color_video = 0;
 	break;
-/* #ifdef WINDOWSNT */
-/*       case 16: */
-/* 	tty->TS_set_foreground = "[%dm"; */
-/* 	tty->TS_set_background = "[%dm"; */
-/* 	tty->TN_max_colors = 16; */
-/* 	tty->TN_no_color_video = 0; */
-/* 	break; */
-/*       case 256: */
-/* 	tty->TN_max_colors = 256; */
-/* 	tty->TS_set_foreground = "[38;5;%dm"; */
-/* 	tty->TS_set_background = "[48;5;%dm"; */
-/* 	tty->TN_no_color_video = 0; */
-/* 	break; */
-/*       case 16777216: */
-/* 	tty->TN_max_colors = 16777216; */
-/* 	tty->TS_set_foreground = "[38;2;%lu;%lu;%lum"; */
-/* 	tty->TS_set_background = "[48;2;%lu;%lu;%lum"; */
-/* 	tty->TN_no_color_video = 0; */
-/* 	break; */
-/* #endif */
+#ifdef WINDOWSNT
+      case 16:
+	tty->TS_set_foreground = "\x1b[%dm";
+	tty->TS_set_background = "\x1b[%dm";
+	tty->TN_max_colors = 16;
+	tty->TN_no_color_video = 0;
+	break;
+      case 256:
+	tty->TN_max_colors = 256;
+	tty->TS_set_foreground = "\x1b[38;5;%dm";
+	tty->TS_set_background = "\x1b[48;5;%dm";
+	tty->TN_no_color_video = 0;
+	break;
+      case 16777216:
+	tty->TN_max_colors = 16777216;
+	tty->TS_set_foreground = "\x1b[38;2;%lu;%lu;%lum";
+	tty->TS_set_background = "\x1b[48;2;%lu;%lu;%lum";
+	tty->TN_no_color_video = 0;
+	break;
+#endif
     }
 }
 
@@ -2331,11 +2331,11 @@ set_tty_color_mode (struct tty_display_info *tty, struct frame *f)
       tty->previous_color_mode = mode;
       tty_setup_colors (tty , mode);
       /*  This recomputes all the faces given the new color definitions. */
-/* #ifdef WINDOWSNT */
-/*       safe_calln (Qw32con_set_up_initial_frame_faces); */
-/* #else */
+#ifdef WINDOWSNT
+      safe_calln (Qw32con_set_up_initial_frame_faces);
+#else
       safe_calln (Qtty_set_up_initial_frame_faces);
-/* #endif */
+#endif
     }
 }
 
@@ -4613,8 +4613,8 @@ use the Bourne shell command 'TERM=...; export TERM' (C-shell:\n\
 		 || ((bg = getenv ("COLORTERM")) != NULL
 		     && strcasecmp (bg, "truecolor") == 0))
 	  {
-	    tty->TS_set_foreground = "\033[%?%p1%{8}%<%t3%p1%d%e38;2;%p1%{65536}%/%d;%p1%{256}%/%{255}%&%d;%p1%{255}%&%d%;m";
-	    tty->TS_set_background = "\033[%?%p1%{8}%<%t4%p1%d%e48;2;%p1%{65536}%/%d;%p1%{256}%/%{255}%&%d;%p1%{255}%&%d%;m";
+	    tty->TS_set_foreground = "\x1b[%?%p1%{8}%<%t3%p1%d%e38;2;%p1%{65536}%/%d;%p1%{256}%/%{255}%&%d;%p1%{255}%&%d%;m";
+	    tty->TS_set_background = "\x1b[%?%p1%{8}%<%t4%p1%d%e48;2;%p1%{65536}%/%d;%p1%{256}%/%{255}%&%d;%p1%{255}%&%d%;m";
 	    tty->TN_max_colors = 16777216;
 	  }
       }
@@ -4669,26 +4669,25 @@ use the Bourne shell command 'TERM=...; export TERM' (C-shell:\n\
     initialize_w32_display (terminal, &width, &height);
 
     /* 24bit RGB support in Windows (10+) Terminal and Console Host
-       Note: Windows interface appears to favor escape /literals/
-        <= C-q C-[ */
+       \x1b <= C-q C-[ */
     tty->TN_no_color_video = 0;
     tty->TN_max_colors = 16777216;
-    tty->TS_orig_pair = "[39m[49m";
-    tty->TS_set_foreground = "[38;2;%lu;%lu;%lum";
-    tty->TS_set_background = "[48;2;%lu;%lu;%lum";
+    tty->TS_orig_pair = "\x1b[39m\x1b[49m";
+    tty->TS_set_foreground = "\x1b[38;2;%lu;%lu;%lum";
+    tty->TS_set_background = "\x1b[48;2;%lu;%lu;%lum";
 
     /* Save default color capabilities */
-    /* tty_default_color_capabilities (tty, 1); */
+    tty_default_color_capabilities (tty, 1);
 
-    tty->TS_enter_bold_mode = "[1m";
-    tty->TS_enter_italic_mode = "[3m";
-    tty->TS_enter_strike_through_mode = "[9m";
-    tty->TS_enter_underline_mode = "[4m";
-    tty->TS_exit_underline_mode = "[24m";
-    tty->TS_enter_reverse_mode = "[7m";
-    tty->TS_exit_attribute_mode = "[0m";
-    tty->TS_cursor_invisible = "[?25l";
-    tty->TS_cursor_visible = "[?25h";
+    tty->TS_enter_bold_mode = "\x1b[1m";
+    tty->TS_enter_italic_mode = "\x1b[3m";
+    tty->TS_enter_strike_through_mode = "\x1b[9m";
+    tty->TS_enter_underline_mode = "\x1b[4m";
+    tty->TS_exit_underline_mode = "\x1b[24m";
+    tty->TS_enter_reverse_mode = "\x1b[7m";
+    tty->TS_exit_attribute_mode = "\x1b[0m";
+    tty->TS_cursor_invisible = "\x1b[?25l";
+    tty->TS_cursor_visible = "\x1b[?25h";
 
     FrameRows (tty) = height;
     FrameCols (tty) = width;
@@ -5250,7 +5249,7 @@ non-nil to enable this optimization.  */);
   defsubr (&Stty_display_pixel_width);
   defsubr (&Stty_display_pixel_height);
 
-#if !defined DOS_NT && !defined HAVE_ANDROID
+#if !defined MSDOS && !defined HAVE_ANDROID
   default_orig_pair = NULL;
   default_set_foreground = NULL;
   default_set_background = NULL;
