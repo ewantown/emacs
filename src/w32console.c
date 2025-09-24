15df60ca95b177b271909e838a1536d8206be01f
ewantown <you@example.com>
2025-09-24 11:56:54 -0700

    remove default to 24bit in setup

--- src/w32console.c
+++ src/w32console.c
@@ -604,9 +604,7 @@ w32con_setup_virtual_terminal (void)
   int out_mode_set = SetConsoleMode (cur_screen, out_mode);
   w32_use_virtual_terminal = w32_use_virtual_terminal && out_mode_set;
 
-  if (w32_use_virtual_terminal)
-    tty_setup_colors (current_tty, 16777216);
-  else
+  if (!w32_use_virtual_terminal && current_tty->TN_max_colors > 16)
     tty_setup_colors (current_tty, 16);
 
   safe_calln (Qtty_set_up_initial_frame_faces);
